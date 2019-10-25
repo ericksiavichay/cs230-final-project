3b2f6bac38c4cfe0065c710aafca870e3e71a414
sgadgil6 <sohamudaygadgil@gmail.com>
2019-10-25 15:55:41 -0700

    Modified st_gcn for hcp data

--- adj/adj.py
+++ adj/adj.py
@@ -1,45 +1,44 @@
 '''Script for Calculating Adjacency / Diagonal matrix
-for half of hemisphere
 '''
 
-import argparse, sys, glob
 import numpy as np
+import glob
 
-
-print("import hcp input data for half")
+print("import hcp input data")
 data = None
-for file in glob.glob('data/*left.npy'):
-	filename = file.split("_")
-	left = np.load(file)
-	right = np.load('_'.join(filename[:-1]) + '_right.npy')
-	d = left + right / 2
-	if data is None:
-		data = d
-	else:
-		data = np.concatenate((d, data), axis=1)
+for file in glob.glob('../data/hcp_tc_npy/*left.npy'):
+    filename = file.split("_")
+    left = np.load(file)
+    if left.shape[1] == 1200:
+        right = np.load('_'.join(filename[:-1]) + '_right.npy')
+        d = np.concatenate((left, right), axis=0)
+        if data is None:
+            data = d
+        else:
+            data = np.concatenate((d, data), axis=1)
 print('sanity check: current data has shape of ' + str(data.shape))
 
 print('compute triangular matrix')
 n_regions = len(d)
-A = np.zeros((n_regions, n_regions))
+A = np.zeros((1, n_regions, n_regions))
 for i in range(n_regions):
-	for j in range(i, n_regions):
-		if i==j: 
-			A[i][j] = 1
-		else:
-			A[i][j] = abs(np.corrcoef(data[i,:], data[j,:])[0][1]) # get value from corrcoef matrix
-			A[j][i] = A[i][j]
+    for j in range(i, n_regions):
+        if i==j:
+            A[0][i][j] = 1
+        else:
+            A[0][i][j] = abs(np.corrcoef(data[i,:], data[j,:])[0][1]) # get value from corrcoef matrix
+            A[0][j][i] = A[0][i][j]
 
 print('compute hat adjacency matrix')
-A_hat = A + np.identity(n_regions)
+A_hat = np.zeros((1, n_regions, n_regions))
+A_hat[0] = A[0] + np.identity(n_regions)
 
 print('compute diagnoal hat')
 D = np.zeros((n_regions, n_regions))
 for i in range(n_regions):
-	D[i][i] = np.sum(A[i])
+    D[i][i] = np.sum(A[0][i])
 
 print('export adjacency matrix')
-np.save('adj_matrix.npy', A) 
+np.save('adj_matrix.npy', A)
 np.save('adj_hat.npy', A_hat)
 np.save('d_hat.npy', D)
-   
